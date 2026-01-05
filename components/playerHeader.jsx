2e48919c9bb2505934315f583c9c41edfe8d1387
Nikita <nikita.lizin98@gmail.com>
2026-01-05 19:13:36 +0100

    Fixed img  path for playerHeader.jsx

--- components/playerHeader.jsx
+++ components/playerHeader.jsx
@@ -24,14 +24,14 @@ function PlayerHeader ({player1,player2,currentPlayer}) {
 
       <div className="player-vs-card" style = {(player1 === currentPlayer) ? activePlayer:waitingPlayer}   > 
         <h3 style = {player1Style} > {player1} </h3>
-        <img src="/assets/X.svg" className="player-card-piece"/>
+        <img src="./assets/X.svg" className="player-card-piece"/>
       </div>
       <div className="vs-container">
         <h3> vs </h3>
       </div>
       <div className="player-vs-card" style = {(player2 === currentPlayer)? activePlayer:waitingPlayer} >
         <h3 style={player2Style}> {player2} </h3>
-        <img src="/assets/O.svg" className="player-card-piece"/>
+        <img src="./assets/O.svg" className="player-card-piece"/>
       </div>
       
       
